25feaf1b91ebe7b173ad91143ecf50795944687f
Ben <robert.ben.thompson@gmail.com>
2020-03-04 14:22:20 -0500

    commentbox npm

--- js/commentbox.js
+++ js/commentbox.js
@@ -1,6 +1,5 @@
 import commentBox from 'commentbox.io';
-// or
-const commentBox = require('commentbox.io');
+
 // or if using the CDN, it will be available as a global "commentBox" variable.
 
 commentBox('5754860016238592-proj');
